74668f2c4e5d740a0ec334a15667b667066a2180
Shaminder Singh <shaminder.qa@gmail.com>
2025-12-03 20:27:22 -0700

    Selenium Lecture Two

--- src/main/java/org/example/SeleniumLectureOne.java
+++ src/main/java/org/example/SeleniumLectureOne.java
@@ -3,6 +3,7 @@ package org.example;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class SeleniumLectureOne {
     public static void main(String[] args) {
